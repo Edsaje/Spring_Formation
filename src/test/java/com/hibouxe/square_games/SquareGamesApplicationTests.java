ee36ff429ac1b389da919c2517c1789158183787
Quentin Beaud <quentin.beaud@hotmail.fr>
2026-09-10 09:42:16 +0200

    First commit

---
+++ src/test/java/com/hibouxe/square_games/SquareGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibouxe.square_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
